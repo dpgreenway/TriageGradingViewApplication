31220762a5f9a056d1b9910db2d7e06d26e09afa
dpgreenway <dpgreenway@bsu.edu>
2018-02-28 14:46:38 -0500

    Initial Commit. Built the first scene.

--- src/cs345/bsu/edu/greenway/triage/project/JavaViewClass.java
+++ src/cs345/bsu/edu/greenway/triage/project/JavaViewClass.java
@@ -3,6 +3,7 @@ package cs345.bsu.edu.greenway.triage.project;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -10,10 +11,12 @@ public class JavaViewClass extends Application{
 
     public void start(Stage primaryStage){
         Button beginButton = new Button();
-        VBox titlePage = new VBox(beginButton);
+        Label initialLabel = new Label();
+        VBox titlePage = new VBox(initialLabel, beginButton);
         Scene scene = new Scene(titlePage, 800, 500);
         primaryStage.setTitle("Reading Triage Grades");
         primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main (String[] args){
