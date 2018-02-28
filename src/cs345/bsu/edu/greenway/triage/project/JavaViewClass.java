cfaa7508d316086f816c8ab1d04556ef516817aa
Danny Greenway <dpgreenway@bsu.edu>
2018-02-28 15:38:28 -0500

    Added successful screen switching into program

--- src/cs345/bsu/edu/greenway/triage/project/JavaViewClass.java
+++ src/cs345/bsu/edu/greenway/triage/project/JavaViewClass.java
@@ -1,26 +1,45 @@
 package cs345.bsu.edu.greenway.triage.project;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class JavaViewClass extends Application{
 
-    public void start(Stage primaryStage){
-        Button beginButton = new Button();
-        Label initialLabel = new Label();
-        VBox titlePage = new VBox(initialLabel, beginButton);
+    private Stage primaryStage;
+
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        Button beginButton = new Button("Learn How to Read My Scores");
+        Label initialLabel = new Label("Reading Triage Grades in Blackboard or Canvas");
+        VBox titlePage = new VBox();
+        titlePage.getChildren().addAll(initialLabel, beginButton);
         Scene scene = new Scene(titlePage, 800, 500);
         primaryStage.setTitle("Reading Triage Grades");
         primaryStage.setScene(scene);
         primaryStage.show();
+        listenForButtonClick(beginButton);
     }
 
-    public static void main (String[] args){
-
+    private void listenForButtonClick(Button button){
+        button.setOnAction(event -> buildScene2());
     }
+
+    private void buildScene2(){
+        Button beginButton = new Button("Learn How to Read My Scores 2");
+        Label initialLabel = new Label("Reading Triage Grades in Blackboard or Canvas 2");
+        VBox titlePage = new VBox();
+        titlePage.getChildren().addAll(beginButton, initialLabel);
+        primaryStage.setTitle("Page 2");
+        primaryStage.setScene(new Scene(titlePage, 800, 500));
+    }
+
+
 
 }
