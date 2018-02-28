6783f999a79f4e50cf82419a6c1359cc1e7ac7e1
Danny Greenway <dpgreenway@bsu.edu>
2018-02-28 16:53:09 -0500

    Added View content inside a second Scene. Made the screen2 method and added the view components to the new scene.

--- src/cs345/bsu/edu/greenway/triage/project/JavaViewClass.java
+++ src/cs345/bsu/edu/greenway/triage/project/JavaViewClass.java
@@ -1,13 +1,14 @@
 package cs345.bsu.edu.greenway.triage.project;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Screen;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class JavaViewClass extends Application{
@@ -17,9 +18,15 @@ public class JavaViewClass extends Application{
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         Button beginButton = new Button("Learn How to Read My Scores");
+        beginButton.setFont(new Font("Serif", 24));
+
         Label initialLabel = new Label("Reading Triage Grades in Blackboard or Canvas");
+        initialLabel.setFont(new Font("Serif", 36));
+
         VBox titlePage = new VBox();
         titlePage.getChildren().addAll(initialLabel, beginButton);
+        titlePage.setAlignment(Pos.CENTER);
+        titlePage.setSpacing(20);
         Scene scene = new Scene(titlePage, 800, 500);
         primaryStage.setTitle("Reading Triage Grades");
         primaryStage.setScene(scene);
@@ -32,12 +39,22 @@ public class JavaViewClass extends Application{
     }
 
     private void buildScene2(){
-        Button beginButton = new Button("Learn How to Read My Scores 2");
-        Label initialLabel = new Label("Reading Triage Grades in Blackboard or Canvas 2");
-        VBox titlePage = new VBox();
-        titlePage.getChildren().addAll(beginButton, initialLabel);
+        Label initialLabel = new Label("Analyze Your Received Grade");
+        initialLabel.setFont(new Font ("Serif", 36));
+
+
+        TextField numerator = new TextField();
+        Label divisionSign = new Label("/");
+        divisionSign.setFont(new Font ("Serif", 36));
+        TextField denomenator = new TextField();
+        HBox fractionInput = new HBox(numerator, divisionSign, denomenator);
+
+        Button generateButton = new Button("Generate Grade");
+        generateButton.setFont(new Font ("Serif", 36));
+        VBox gradeAnalyzer = new VBox(initialLabel, fractionInput, generateButton);
+
         primaryStage.setTitle("Page 2");
-        primaryStage.setScene(new Scene(titlePage, 800, 500));
+        primaryStage.setScene(new Scene(gradeAnalyzer, 800, 500));
     }
 
 
